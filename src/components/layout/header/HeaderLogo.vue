01ebd7ca8673c0a6d4d05736416b5429005e9bf3
carlossilvestri <carlosarmando99@hotmail.com>
2025-11-20 17:46:46 -0400

    first commit

---
+++ src/components/layout/header/HeaderLogo.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-link to="/" class="lg:hidden">
+    <img class="dark:hidden" src="@/assets/img/logos/logo-ipe-arequipa.png" alt="Logo" />
+    <img class="hidden dark:block" src="@/assets/img/logos/logo-ipe-arequipa.png" alt="Logo" />
+  </router-link>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
